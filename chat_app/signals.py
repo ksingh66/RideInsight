35ce0a86b3915f7f0159f014e9387d6bac1cd62a
Karman Singh <karman17singh@gmail.com>
2025-01-07 21:52:07 -0800

    added admin emails

--- chat_app/signals.py
+++ chat_app/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
@@ -7,30 +7,51 @@ from django.utils.html import strip_tags
 
 from .models import CustomUser
 
+@receiver(post_save, sender=CustomUser)
+def notify_admin_new_user(sender, instance, created, **kwargs):
+    """
+    Signal handler to notify admin when a new user is created.
+    """
+    if created:
+        # Prepare email content for admin notification
+        admin_context = {
+            'user_email': instance.email,
+            'company_name': instance.company_name,
+            'full_name': f"{instance.first_name} {instance.last_name}",
+            'date_joined': instance.date_joined.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        
+        # Render admin notification email
+        html_message = render_to_string('emails/new_user_admin_notification.html', admin_context)
+        plain_message = strip_tags(html_message)
+        
+        # Send email to admin
+        send_mail(
+            subject=f'New User Registration: {instance.email}',
+            message=plain_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.ADMIN_EMAIL],  # Make sure to define ADMIN_EMAIL in settings
+            html_message=html_message,
+            fail_silently=False,
+        )
+
 @receiver(pre_save, sender=CustomUser)
 def send_approval_email(sender, instance, **kwargs):
     """
     Signal handler to send an email when a user's approval status changes.
-    Compares the current instance with the database instance to detect changes.
     """
     try:
-        # Get the current database instance
         current_user = CustomUser.objects.get(pk=instance.pk)
-        
-        # Check if is_approved status has changed
         if current_user.is_approved != instance.is_approved:
-            # Prepare email content
             context = {
                 'user_email': instance.email,
                 'company_name': instance.company_name,
                 'status': 'approved' if instance.is_approved else 'unapproved'
             }
             
-            # Render email templates
             html_message = render_to_string('emails/approval_status.html', context)
             plain_message = strip_tags(html_message)
             
-            # Send email
             send_mail(
                 subject='Your Account Status Has Been Updated',
                 message=plain_message,
@@ -41,5 +62,4 @@ def send_approval_email(sender, instance, **kwargs):
             )
             
     except CustomUser.DoesNotExist:
-        # This is a new user being created, no need to send approval email
-        pass
+        pass  # This is a new user being created
